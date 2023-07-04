a13f5b611ca5afa86a3b56944ed055b05de8ae4f
Joshua Ibrom <joshuaibrom@gmail.com>
2023-07-04 18:31:13 -0500

    Setup the main app shell

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,20 @@
 
   // Finally, your application's global stylesheet (sometimes labeled 'app.css')
   import '../app.postcss';
+
+  import {AppShell, AppBar} from '@skeletonlabs/skeleton'
 </script>
 
-<slot />
+<AppShell regionPage="relative" slotPageHeader="sticky top-0 z-10">
+  <svelte:fragment slot="pageHeader">
+    <AppBar>Sk3p7ic's Blog</AppBar>
+  </svelte:fragment>
+
+  <slot />
+
+  <svelte:fragment slot="footer">
+    <footer class="py-4 text-center">
+      &copy; Joshua Ibrom, {new Date().getFullYear()}
+    </footer>
+  </svelte:fragment>
+</AppShell>
