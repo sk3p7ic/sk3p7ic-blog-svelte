8579de67d11d3c6325052c80cd8b059221f38801
Joshua Ibrom <joshuaibrom@gmail.com>
2023-07-04 14:30:10 -0500

    Added endpoint to get listing of all files

---
+++ src/routes/api/all-posts/+server.ts
@@ -0,0 +1,34 @@
+import type { RequestHandler } from '@sveltejs/kit';
+import { readdirSync, readFileSync, statSync } from 'fs';
+import { join } from 'path';
+import { unified } from 'unified';
+import remarkParse from 'remark-parse';
+import remarkStringify from 'remark-stringify';
+import remarkFrontmatter from 'remark-frontmatter';
+import remarkParseFrontmatter from 'remark-parse-frontmatter';
+
+const basePath = join('./src', 'content');
+
+const getContentFilenames = (): string[] => readdirSync(basePath);
+
+const stripFilenameExtension = (filename: string) => filename.replace('.md', '');
+
+const parseMarkdownFile = (slug: string) => {
+	const filename = join(process.cwd(), basePath, slug + '.md');
+	const dateCreated = statSync(filename).birthtime;
+	const dateModified = statSync(filename).mtime;
+	// TODO: Add type for frontmatter
+	const frontmatter = unified()
+		.use(remarkParse)
+		.use(remarkFrontmatter)
+		.use(remarkParseFrontmatter)
+		.use(remarkStringify)
+		.processSync(readFileSync(filename)).data?.frontmatter;
+	return { dateCreated, dateModified, frontmatter };
+};
+
+export const GET = (async () => {
+	const filenames = getContentFilenames();
+	const posts = filenames.map(stripFilenameExtension).map(parseMarkdownFile);
+	return new Response(JSON.stringify(posts));
+}) satisfies RequestHandler;
