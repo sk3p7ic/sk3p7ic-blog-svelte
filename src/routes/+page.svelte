7c33bf7d97e474949f01e915cab1174af397f658
Joshua Ibrom <joshuaibrom@gmail.com>
2023-07-04 14:51:51 -0500

    Confirmed that the all-posts endpoint works

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	export let data: PageData;
+	console.log(data);
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+{#each data.allPosts as post}
+	<h1>{post.frontmatter.title}</h1>
+	<p>{post.frontmatter.description}</p>
+{/each}
