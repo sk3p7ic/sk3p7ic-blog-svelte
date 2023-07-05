15cab04b311e0b90efcd6dcd60dcf549e40b0ccf
Joshua Ibrom <joshuaibrom@gmail.com>
2023-07-05 00:16:25 -0500

    Worked on rendering of more components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,26 +1,32 @@
 <script>
-  // Your selected Skeleton theme:
-  import '@skeletonlabs/skeleton/themes/theme-vintage.css';
+	// Your selected Skeleton theme:
+	import '@skeletonlabs/skeleton/themes/theme-vintage.css';
 
-  // This contains the bulk of Skeletons required styles:
-  import '@skeletonlabs/skeleton/styles/skeleton.css';
+	// This contains the bulk of Skeletons required styles:
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
 
-  // Finally, your application's global stylesheet (sometimes labeled 'app.css')
-  import '../app.postcss';
+	// Finally, your application's global stylesheet (sometimes labeled 'app.css')
+	import '../app.postcss';
 
-  import {AppShell, AppBar} from '@skeletonlabs/skeleton'
+	import hljs from 'highlight.js';
+	import 'highlight.js/styles/github-dark.css';
+
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import { storeHighlightJs } from '@skeletonlabs/skeleton';
+
+	storeHighlightJs.set(hljs);
 </script>
 
 <AppShell regionPage="relative" slotPageHeader="sticky top-0 z-10">
-  <svelte:fragment slot="pageHeader">
-    <AppBar>Sk3p7ic's Blog</AppBar>
-  </svelte:fragment>
+	<svelte:fragment slot="pageHeader">
+		<AppBar>Sk3p7ic's Blog</AppBar>
+	</svelte:fragment>
 
-  <slot />
+	<slot />
 
-  <svelte:fragment slot="footer">
-    <footer class="py-4 text-center">
-      &copy; Joshua Ibrom, {new Date().getFullYear()}
-    </footer>
-  </svelte:fragment>
+	<svelte:fragment slot="footer">
+		<footer class="py-4 text-center">
+			&copy; Joshua Ibrom, {new Date().getFullYear()}
+		</footer>
+	</svelte:fragment>
 </AppShell>
--- src/routes/post/[slug]/ArticleRenderer.svelte
+++ src/routes/post/[slug]/ArticleRenderer.svelte
@@ -2,27 +2,66 @@
 	import type { AST } from '$lib/types';
 	import { prependFilenameToCode } from '$lib/util';
 	import { CodeBlock } from '@skeletonlabs/skeleton';
+	import Katex from 'svelte-katex';
+	import ArticleListRenderer from './ArticleListRenderer.svelte';
+	import { unified } from 'unified';
+	import remarkMath from 'remark-math';
+	import rehypeKatex from 'rehype-katex';
+	import rehypeStringify from 'rehype-stringify/lib';
+	import remarkHtml from 'remark-html';
+	import remarkRehype from 'remark-rehype/lib';
+	import rehypeRemark from 'rehype-remark/lib';
 
 	export let ast: AST;
 	const { children } = ast;
+	// TODO: Split at the code blocks and process them separately
+	let modifiedChildren = children.filter((child) => child.type !== 'code');
+	ast.children = modifiedChildren;
+	const markup = unified()
+		.use(remarkMath)
+		.use(remarkRehype)
+		.use(rehypeKatex)
+		.use(rehypeStringify)
+		.use(rehypeRemark)
+		.use(remarkHtml, { sanitize: false })
+		.stringify(ast);
+	console.log(markup);
 </script>
 
 {#each children as child}
 	{#if child.type === 'heading'}
-		<h1>{child.data}</h1>
+		<a class={`h${child.depth} group`} href={`#${child.children[0].value}`}>
+			<span id={`${child.children[0].value}`}>{child.children[0].value}</span>
+			<span class="text-gray-500 opacity-0 group-hover:opacity-100 transition-opacity">#</span>
+		</a>
 	{/if}
+
 	{#if child.type === 'paragraph'}
-		<p>{child.data}</p>
+		<p>
+			{#each child.children as pchild}
+				{#if pchild.type === 'text'}
+					{pchild.value}
+				{:else if pchild.type === 'inlineMath'}
+					<span class="math inline-math">
+						<Katex>{pchild.value}</Katex>
+					</span>
+				{/if}
+			{/each}
+		</p>
 	{/if}
+
 	{#if child.type === 'code'}
 		<CodeBlock
 			language={child.lang || ''}
 			code={prependFilenameToCode(child.value, child.meta || '')}
 		/>
 	{/if}
+
 	{#if child.type === 'list'}
 		<ul>
-			<li>List goes here.</li>
+			{#each child.children as listchild}
+				<ArticleListRenderer ast={listchild} />
+			{/each}
 		</ul>
 	{/if}
 {/each}
