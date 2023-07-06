de9c1ad9967b26f4366763cc1c424c9e6fa1ab7f
Joshua Ibrom <joshuaibrom@gmail.com>
2023-07-06 09:19:04 -0500

    Added author info and categories to newlyt-styled article pages

--- src/routes/post/[slug]/+page.svelte
+++ src/routes/post/[slug]/+page.svelte
@@ -4,6 +4,7 @@
 	import ArticleRenderer from './ArticleRenderer.svelte';
 	import { TableOfContents } from '@skeletonlabs/skeleton';
 	import { onMount } from 'svelte';
+	import { formatDateString } from '$lib/util';
 
 	export let data: PageData;
 	let post: PostType = data.post;
@@ -25,16 +26,34 @@
 	<title>{post.frontmatter.title}</title>
 </svelte:head>
 
-<div class="mx-12 my-12">
+<div class="p-4 lg:mx-12 lg:my-12">
 	<div class="card">
 		<header class="card-header">
 			<h1 class="h1">{post.frontmatter.title}</h1>
-			<p class="text-gray-500 text-sm">{new Date(post.dateCreated)}</p>
+			<p class="text-gray-500 text-sm">
+				Created {formatDateString(post.dateCreated)}
+				{post.dateModified && ` | Updated ${formatDateString(post.dateModified)}`}
+			</p>
+			<div class="flex flex-row flex-wrap gap-2">
+				{#if post.frontmatter.categories}
+					{#each post.frontmatter.categories as category}
+						<span class="badge variant-filled">{category}</span>
+					{/each}
+				{/if}
+			</div>
 		</header>
 		<section class="p-4">
 			<div id="toc" class="py-2 border-y" />
 			<div id="the-article"><ArticleRenderer ast={post.ast} /></div>
 		</section>
-		<footer class="card-footer">Author Info</footer>
+		<footer class="card-footer border-t pt-4">
+			<h2 class="h2">Author Info</h2>
+			<h3 class="h3">Joshua Ibrom</h3>
+			<p>
+				I'm a computer science student at Texas A&amp;M University - San Antonio. I'm also a web
+				developer and general tech enthusiast. You may find more about me and the work that I do at
+				<a href="https://joshuaibrom.com" class="anchor">joshuaibrom.com</a>.
+			</p>
+		</footer>
 	</div>
 </div>
